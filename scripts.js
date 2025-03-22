b94fe5ef338301ce3bbccc5db91c240ce06f099f
robert koopedi <koopedi05@gmail.com>
2025-03-22 19:19:21 +0200

    added validateInput function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+let timeoutId;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -7,3 +8,16 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
 });
+
+function validateInput(dividend, divider) {
+  if(!dividend || !divider) {
+    return "Division not performed.Both values are required in inputs. Try again";
+  }
+  if(divider == 0) {
+    console.error("Division by zero error");
+    return "Division not performed. Invalid number provided. Try again";
+  
+  }
+  return null; 
+}
+
